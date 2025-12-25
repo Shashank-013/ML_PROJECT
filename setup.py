e0766860f1ed845a1c81ede9eb1d4e479f1fc558
Shashank-013 <shashankmaurya013@gmail.com>
2025-12-25 23:43:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List 
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path: str) -> List[str]:
+    '''
+   this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return [req.replace("\n","") for req in requirements]
+
+
+        
+    return requirements
+
+setup(
+    name='ML_PROJECT',
+    version='0.0.1',
+    author='Shashank',
+    author_email='amgodboyi@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+)
